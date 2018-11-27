2e0a058504a86be3d72d1a9d3bd6b38991ad729c
Darrin Howell <dbhowell09@gmail.com>
2018-11-27 09:34:53 -0800

    developed pluralize method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        String testPlural = pluralize("test", 1);
+        System.out.println(testPlural);
+    }
+
+    // method takes in a string and an int and pluralizes that string
+    // if the int is 0 or greater than 1
+    public static String pluralize(String noun, int numNouns){
+
+        // if numNouns == 0 OR numNouns > 1, then add an "s" to noun
+        if(numNouns == 0 || numNouns > 1){
+            noun += "s";
+        }
+
+        return noun;
     }
 }
