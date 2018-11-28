956429fbd678a09cfca4aea7eed1c1b4db30b570
Darrin Howell <dbhowell09@gmail.com>
2018-11-28 10:13:43 -0800

    completed roll method and test

--- basiclibrary/src/main/java/Main.java
+++ basiclibrary/src/main/java/Main.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
+
 // class that will run our library methods
 public class Main {
     public static void main(String[] args){
-        System.out.println(Library.someLibraryMethod());
+        System.out.println(Arrays.toString(Library.roll(8)));
     }
 }
--- basiclibrary/src/test/java/LibraryTest.java
+++ basiclibrary/src/test/java/LibraryTest.java
@@ -5,8 +5,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class LibraryTest {
-    @Test public void testSomeLibraryMethod() {
+    @Test public void testRoll() {
         Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+        int n = 8;
+        int[] resultArr = classUnderTest.roll(n);
+        assertEquals(n, resultArr.length);
     }
 }
