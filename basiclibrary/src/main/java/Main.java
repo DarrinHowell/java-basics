75279ae4c7c8060828bf19b3f6cf8b4def88d2be
Darrin Howell <dbhowell09@gmail.com>
2018-11-29 10:34:12 -0800

    completed weather method

--- basiclibrary/src/main/java/Main.java
+++ basiclibrary/src/main/java/Main.java
@@ -3,16 +3,29 @@ import java.util.Arrays;
 // class that will run our library methods
 public class Main {
     public static void main(String[] args){
-        System.out.println(Arrays.toString(Library.roll(8)));
+//        System.out.println(Arrays.toString(Library.roll(8)));
+//
+//        System.out.println();
+//
+//        int[] testArr = {3, 4, 5, 6, 7, 8, 8};
+//        System.out.println(Library.containsDuplicates(testArr));
+//
+//        System.out.println();
+//
+//        int[] testArr2 = {1, 2, 3, 4, 5, 6, 7};
+//        System.out.println(Library.averageFinder(testArr2));
 
         System.out.println();
 
-        int[] testArr = {3, 4, 5, 6, 7, 8, 8};
-        System.out.println(Library.containsDuplicates(testArr));
+        int[][] weeklyMonthTemperatures = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
 
-        System.out.println();
+        Library.analyzeWeather(weeklyMonthTemperatures);
 
-        int[] testArr2 = {1, 2, 3, 4, 5, 6, 7};
-        System.out.println(Library.averageFinder(testArr2));
+
     }
 }
