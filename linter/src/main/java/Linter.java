b0c0bcfea26fcd41d5a109bf6bf12b4776995bcf
Darrin Howell <dbhowell09@gmail.com>
2018-12-02 17:35:03 -0800

    printing out error messages when semicolons not present

--- linter/src/main/java/Linter.java
+++ linter/src/main/java/Linter.java
@@ -15,16 +15,36 @@ public class Linter {
         alertSemicolons(jsPath);
     }
 
+    // method prints an error message when a semicolon is not terminating a line when it should be
+    // method takes in a path and reads in a javascript file
     public static void alertSemicolons(Path jsPath){
+
         try {
 
             BufferedReader reader = Files.newBufferedReader(jsPath);
             String newLine;
 
+            int lineNum = 0;
+
+            // while we haven't reached the end of the file, read the file
             while((newLine = reader.readLine()) != null){
-                System.out.println(newLine);
+                lineNum++;
+
+                // if the line is not empty, do some work to check if ; are in the right places
+                if(newLine.length() > 0) {
+                    char lastChar = newLine.charAt(newLine.length()-1);
+
+                    // if semicolons are not present where they should be, print an error
+                    if(lastChar != ';' && lastChar != '{' && lastChar != '}' && !newLine.contains("if") &&
+                        !newLine.contains("else")){
+
+                        System.out.println("Line " + lineNum + ": Missing semicolon.");
+
+                    }
+                }
             }
 
+        // if we don't properly read a file, print out an error message
         } catch (IOException e) {
 
             System.out.println(e);
