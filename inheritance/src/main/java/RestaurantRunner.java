1a59557d728e4a9d1f4aeff640c40b981f85a50b
Darrin Howell <dbhowell09@gmail.com>
2018-12-05 12:00:04 -0800

    first draft of addReview for shops

--- inheritance/src/main/java/RestaurantRunner.java
+++ inheritance/src/main/java/RestaurantRunner.java
@@ -18,15 +18,28 @@ public class RestaurantRunner {
 //
 //        System.out.println("This is the avg numStars after the review: " + newRestaurant.toString());
 
-        Shop testShop = new Shop("nordstroms", "high-end retail", "$$$");
-        Shop testShop2 = new Shop("foot-locker", "shoe store", "$$");
-        Shop testShop3 = new Shop("REI", "outdoor retail", "$$$");
-
-
-
-
+        Shop testShop = new Shop("nordstroms", "high-end retail");
+        Shop testShop2 = new Shop("foot-locker", "shoe store");
+        Shop testShop3 = new Shop("REI", "outdoor retail");
 
         System.out.println(testShop);
+
+        System.out.println();
+
+
+        Review shopReview1 = new Review("amazing quality", "D$", 1.0);
+        Review shopReview2 = new Review("amazing quality", "D$", 8.0);
+        Review shopReview3 = new Review("amazing quality", "D$", 10.0);
+        Review shopReview4 = new Review("amazing quality", "D$", 5.0);
+
+        testShop2.addReview(shopReview1);
+        testShop2.addReview(shopReview2);
+        testShop2.addReview(shopReview3);
+        testShop2.addReview(shopReview4);
+
+        System.out.println("this is the avg num stars for testShop2: " + testShop2.getReview());
+
+
 
     }
 }
--- inheritance/src/main/java/Review.java
+++ inheritance/src/main/java/Review.java
@@ -3,11 +3,21 @@ public class Review {
     private String author;
     private int numStars;
 
+    // adding functionality for shops
+    private double numDollarSigns;
+
     public Review(String body, String author, int numStars){
         this.body = body;
         this.author = author;
         this.numStars = numStars;
     }
+
+    public Review(String body, String author, double numDollarSigns){
+        this.body = body;
+        this.author = author;
+        this.numDollarSigns = numDollarSigns;
+    }
+
 
     public String toString(){
         return Integer.toString(this.numStars);
@@ -15,6 +25,10 @@ public class Review {
 
     public int getStars(){
         return this.numStars;
+    }
+
+    public double getNumDollarSigns(){
+        return this.numDollarSigns;
     }
 
 
